ff447b0a442ca3399ecb06de9eb868e12489ebfd
Olga Zhivaeva <olga.zhiv@yandex.ru>
2023-02-12 21:48:15 +0300

    Изменены параметры функции

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 class SuperHero:
     def __init__(self, id):
         self.id = id
-    def get_intelligence(self, id):
+    def get_intelligence(self):
         """Метод возвращает значение intelligence"""
-        self.id = id
         url_to_request = BASEURL + f'/powerstats/{self.id}.json'
         response = requests.get(url=url_to_request)
         return response.json()['intelligence']
@@ -14,9 +13,9 @@ if __name__ == '__main__':
     hulk = SuperHero('332')
     thanos = SuperHero('655')
     cap_am = SuperHero('149')
-    hulk_int = hulk.get_intelligence('332')
-    thanos_int = thanos.get_intelligence('655')
-    cap_am_int = cap_am.get_intelligence('149')
+    hulk_int = hulk.get_intelligence()
+    thanos_int = thanos.get_intelligence()
+    cap_am_int = cap_am.get_intelligence()
     print(f'Hulk intelligence {hulk_int}')
     print(f'Thanos intelligence {thanos_int}')
     print(f'Captain America intelligence {cap_am_int}')
